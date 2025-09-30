c41d6d139ec2e8fb646cc7fa557e8329161bc597
riyaa3khede05 <riyatinkhede3@gmail.com>
2025-09-30 19:02:28 +0530

    Create employee.c file uploaded

---
+++ employee.c
@@ -0,0 +1,14 @@
+<Students>
+    <Student>
+        <RollNo>18</RollNo>
+        <Name>Riya</Name>
+        <Course>B.TECH</Course>
+        <Email>riya@example.com</Email>
+    </Student>
+    <Student>
+        <RollNo>5</RollNo>
+        <Name>ram</Name>
+        <Course>B.Sc IT</Course>
+        <Email>ram@example.com</Email>
+    </Student>
+</Students>
